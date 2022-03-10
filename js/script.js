bfcba70ee2900f403cf7827ca489be1089ee5714
marshall-demars <99354910+marshall-demars@users.noreply.github.com>
2022-03-10 18:28:46 +0000

    changed 3 files

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Created by: Marshall
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+/**
+ * Do basic math.
+ */
+function doMathClicked() {
+  // this function does basic math
+document.getElementById('multiply-math').innerHTML = '<p>5 × 3 = ' + (5 * 3) + '</p>'
