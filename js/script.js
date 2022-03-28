c9c297b5838a4309991fbff68539696ce1753c57
marshall-demars <99354910+marshall-demars@users.noreply.github.com>
2022-03-28 16:59:40 +0000

    1 file

--- js/script.js
+++ js/script.js
@@ -7,6 +7,6 @@
  */
 function doMathClicked() {
   // this function does basic math
-document.getElementById('area-math').innerHTML = '<p>The area is:' + (5 * 3) + '</p>'
-document.getElementById('perimeter-math').innerHTML = '<p>The perimeter is:' + (2 * (3 + 5)) + '</p>'
+  document.getElementById('area-math').innerHTML = '<p>The area is: ' + (5 * 3) + ' cm²</p>'
+  document.getElementById('perimeter-math').innerHTML = '<p>The perimeter is: ' + (2 * (3 + 5)) + ' cm</p>'
 }
